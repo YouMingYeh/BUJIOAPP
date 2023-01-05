83e9b156844de2cf6e1df6a9261325c89bccedbd
yehyouming <b10705052@ntu.edu.tw>
2023-01-06 03:36:27 +0800

    .

--- server/index.js
+++ server/index.js
@@ -36,10 +36,10 @@ app.get("/api", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   
-  app.use(express.static(path.join(__dirname, "../client", "build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
-  });
+  // app.use(express.static(path.join(__dirname, "../client", "build")));
+  // app.get("/*", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  // });
   
 }
 
